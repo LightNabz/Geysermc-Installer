1186537235cb02ca20a9a43ad1240badc57fea93
LightNabz <87846302+LightNabz@users.noreply.github.com>
2022-04-23 11:23:12 +0800

    Update geyserinstaller.sh

--- geyserinstaller.sh
+++ geyserinstaller.sh
@@ -1,26 +1,26 @@
 echo -e "\e[1mWelcome to Geysermc Installer!"
 
 #Installing
-read -p "Do you want install Geysermc? (y/n)" yn
+read -p "Do you want install Geysermc? (y/n): " yn
 
     case $yn in
         [yY] ) echo Start Downloading, This may take a few minutes..;;
         [nN] ) echo Cancel process..;
                exit;;
-           * ) echo Invalid input, Please input (y/n)..;
+           * ) echo Invalid input, Please input y/n..;
                exit 1;;
 esac
 
     cd ~
-    apt install wget &> /dev/null
-    apt install nano &> /dev/null
-    apt install openjdk-17 &> /dev/null
+    apt install wget -y &> /dev/null
+    apt install nano -y &> /dev/null
+    apt install openjdk-17 -y &> /dev/null
     wget --no-check-certificate https://ci.opencollab.dev//job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/standalone/target/Geyser.jar &> /dev/null
     mkdir Geysermc &> /dev/null
     mv Geyser.jar ~/Geysermc &> /dev/null
-    echo nano ~/Geysermc/config.yml >> $PREFIX/bin/geyser-edit &> /dev/null
+    echo nano ~/Geysermc/config.yml >> $PREFIX/bin/geyser-edit 
     chmod +x $PREFIX/bin/geyser-edit &> /dev/null
-    echo java -Xmx1024M -Xms1024M -jar ~/Geysermc/Geyser.jar >> $PREFIX/bin/geyser-start &> /dev/null
+    echo java -Xmx1024M -Xms1024M -jar ~/Geysermc/Geyser.jar >> $PREFIX/bin/geyser-start
     chmod +x $PREFIX/bin/geyser-start &> /dev/null
     echo ""
 #Fin(n)ish
