1f15bd23e70d664254dca5c537e38a1e053499c4
LightNabz <87846302+LightNabz@users.noreply.github.com>
2022-06-10 18:05:29 +0800

    Create geyserupdater.sh

---
+++ geyserupdater.sh
@@ -0,0 +1,33 @@
+#credit
+echo -e "\e[34m##################################################"
+echo -e "\e[34m#                                                #"
+echo -e "\e[34m#                                                #"
+echo -e "\e[34m#                MADE BY LIGHNABZ                #"
+echo -e "\e[34m#                                                #"
+echo -e "\e[34m#                                                #"
+echo -e "\e[34m##################################################"
+
+echo -e "\e[0m\e[1mWelcome to Geysermc Updater!"
+
+#Installing
+read -p "Do you want update Geysermc? (y/n): " yn
+
+    case $yn in
+        [yY] ) echo Start Updating, This may take a few minutes..;;
+        [nN] ) echo Cancel process..;
+               exit;;
+           * ) echo Invalid input, Please input y/n..;
+               exit 1;;
+esac
+cd ~/Geysermc
+rm -rf Geyser.jar &> /dev/null
+wget --no-check-certificate https://ci.opencollab.dev//job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/standalone/target/Geyser.jar &> /dev/null
+cd ~
+
+#Fin(n)ish
+
+    echo -e "\e[92mGeysermc has successfully Updated!"
+    echo ""
+    echo -e "\e[0mIf you get some problem, PM me on Discord: \e[4m\e[94mLightNabz#9268"
+    
+    echo -e "\e[0m"
